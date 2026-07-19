2fcb88cc5d873a98f0a8f4d8703b3aa00f3ff660
Aapo Attila <aapo.attila@windowslive.com>
2026-07-19 17:54:07 +0300

    add biome, set up canvas resizing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div>
-</section>
-`
+const canvas: HTMLCanvasElement = document.getElementById('archiveCanvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
 
+const syncCanvasResolution = () => {
+  let visualWidth = canvas.clientWidth
+  let visualHeight = canvas.clientHeight
+
+  let scaleFactor = window.devicePixelRatio || 1
+
+  canvas.width = visualWidth * scaleFactor
+  canvas.height = visualHeight * scaleFactor
+
+  ctx.scale(scaleFactor, scaleFactor)
+  ctx.imageSmoothingEnabled = false
+
+
+  //TODO: tigger redrawing grid here
+}
+
+window.addEventListener('resize', syncCanvasResolution)
+
+syncCanvasResolution()
